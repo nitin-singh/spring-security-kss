b6dca227d1a31b3e5309f058e2f692d142329509
Nitin Kumar Singh <nitin.kumar@tothenew.com>
2018-01-20 15:05:41 +0530

    Implemented persistent rememeber me

--- src/main/java/com/springmvc/config/SpringSecurityConfig.java
+++ src/main/java/com/springmvc/config/SpringSecurityConfig.java
@@ -2,11 +2,16 @@ package com.springmvc.config;
 
 import com.springmvc.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,6 +21,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    DataSource dataSource;
 
     @Autowired
     LogoutSuccess logoutSuccess;
@@ -35,13 +43,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .rememberMe()
-                .tokenValiditySeconds(60)
-                .rememberMeCookieName("my-cookie")
-                .rememberMeParameter("remember")
+                .tokenRepository(persistentTokenRepository())
                 .and().formLogin().successHandler(authenticationSuccess).loginPage("/login").permitAll()
                 .loginProcessingUrl("/loginUrl")
                 .and()
                 .logout().permitAll().logoutRequestMatcher(new AntPathRequestMatcher("/doLogout", "GET")).logoutSuccessHandler(logoutSuccess)
                 .and().csrf().disable();
+    }
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 }
